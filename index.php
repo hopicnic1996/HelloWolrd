49fb182bda1d943c434238e992d1f9aeff7f1e55
Jayden <48559341+hopicnic1996@users.noreply.github.com>
2019-10-22 20:13:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,40 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-</head>
+<html>
 <body>
-	<?php echo "<p>Hello trung </p>";?>
+
+<h1>My first PHP page</h1>
+
+<?php
+	echo "Show all rows from Postgres Database";
+	
+	//Refere to database 
+	$db = parse_url(getenv("DATABASE_URL"));
+	$pdo = new PDO("pgsql:" . sprintf(
+	    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+	    $db["host"],
+	    $db["port"],
+	    $db["user"],
+	    $db["pass"],
+	    ltrim($db["path"], "/")
+	));
+	//you sql query
+	$sql = "SELECT studentname, course FROM registercourse";
+	$stmt = $pdo->prepare($sql);
+	//execute the query on the server and return the result set
+	$stmt->setFetchMode(PDO::FETCH_ASSOC);
+	$stmt->execute();
+	$resultSet = $stmt->fetchAll();
+	//display the data 
+?>
+<ul>
+	<?php
+		foreach ($resultSet as $row) {
+			echo "<li>" .
+				$row["studentname"] . '--'. $row["course"]
+			. "</li>";
+		}
+	?>
+</ul>
+
 </body>
 </html>
